7aab51772d717e25ca03fb20fdf205b98b84a015
fabriciofl <fabriciofl.dev@gmail.com>
2022-05-30 18:45:47 -0300

    feature: store tasks endpoint

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -13,8 +14,18 @@ class TaskController extends Controller
     }
 
     public function store(Request $request, string $id) : mixed
-    {
-        return [];
+    {   
+        $payload = [
+            'author' => $request->input('author'),
+            'task' => $request->input('task'),
+            'description' => $request->input('description')
+        ];
+        if($payload['author'] != null && $payload['task'] != null && $payload['description'] != null)
+        {
+            Task::create($payload);
+            return ['message' => 'created'];
+        }
+        return ['message' => 'wrong entry'];
     }
 
     public function updateById(Request $request, string $id) : mixed
